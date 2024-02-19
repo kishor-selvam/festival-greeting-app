ba7a39e7b6046c452805495eb42a586b26a46284
Kishor S <kishorsbca@gmail.com>
2024-02-19 17:29:33 +0530

    added shared link

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -3,3 +3,15 @@ import { Fireworks } from "fireworks-js";
 const fireworksEl = document.querySelector("#fireworks");
 const fireworks = new Fireworks(fireworksEl, {});
 fireworks.start();
+
+const url = new URL(window.location.href);
+const Name = url.searchParams.get("name");
+
+window.document.title = "Greeting By " + Name;
+console.log(Name);
+if (Name) {
+  let userName = document.querySelector("#greeternameid");
+  userName.textContent = Name;
+} else {
+  window.location.href = "/";
+}
