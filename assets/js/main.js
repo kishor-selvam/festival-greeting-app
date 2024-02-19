c8e9113ea87125683960f44138df9a5c7698e701
Kishor S <kishorsbca@gmail.com>
2024-02-19 17:42:03 +0530

    bug fixed #2

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,8 @@ const form = document.querySelector("form#greeterName");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.dir(e.target[0].value);
   const greeterName = e.target[0].value;
-  //   console.log("Submitted");
+  console.log("Submitted");
   window.location.href =
     "/festival-greeting-app/greeting.html?name=" + greeterName;
 });
